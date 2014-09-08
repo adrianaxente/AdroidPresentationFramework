a0c404345262832fbf7509baca01976b34db5231
Axente Adrian <adrianaxente@yahoo.com>
2014-09-09 01:15:18 +0300

    Incipient list notifications support

---
+++ src/com/axy/misc/IGetObject.java
@@ -0,0 +1,8 @@
+package com.axy.misc;
+
+/**
+ * Created by adrianaxente on 08.09.2014.
+ */
+public interface IGetObject<T> {
+    T getObject();
+}
--- src/com/axy/presentation/observable/colections/ListObservableWrapper.java
+++ src/com/axy/presentation/observable/colections/ListObservableWrapper.java
@@ -1,5 +1,8 @@
 package com.axy.presentation.observable.colections;
 
+import android.util.Log;
+import com.axy.misc.IGetObject;
+
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -12,7 +15,7 @@ public class ListObservableWrapper<T> implements  IObservableList<T>
 {
     // <editor-fold description="Fields">
 
-    private List<T> _wrappedList;
+    private IGetObject<List<T>> _wrappedGetter;
 
     private CollectionChangedEvent _collectionChangedEvent = new CollectionChangedEvent();
 
@@ -20,12 +23,27 @@ public class ListObservableWrapper<T> implements  IObservableList<T>
 
     // <editor-fold description="Constructor">
 
-    public ListObservableWrapper(List<T> wrappedList) throws IllegalArgumentException
+    public ListObservableWrapper(final List<T> wrappedList)
+            throws IllegalArgumentException
     {
         if (wrappedList == null)
             throw new IllegalArgumentException("wrappedList");
 
-        this._wrappedList = wrappedList;
+        this._wrappedGetter = new IGetObject<List<T>>() {
+            @Override
+            public List<T> getObject() {
+                return  wrappedList;
+            }
+        };
+    }
+
+    public ListObservableWrapper(IGetObject<List<T>> wrappedListGetter)
+            throws IllegalArgumentException
+    {
+        if (wrappedListGetter == null)
+            throw new IllegalArgumentException("wrappedList");
+
+        this._wrappedGetter = wrappedListGetter;
     }
 
     // </editor-fold>
@@ -43,146 +61,153 @@ public class ListObservableWrapper<T> implements  IObservableList<T>
 
     @Override
     public void add(int location, T object) {
-        this._wrappedList.add(location, object);
+        this.getWrappedList().add(location, object);
         this._collectionChangedEvent.fire(new CollectionChangedEventArgs());
     }
 
     @Override
     public boolean add(T object) {
-        boolean result = this._wrappedList.add(object);
+
+        boolean result = this.getWrappedList().add(object);
+
         this._collectionChangedEvent.fire(new CollectionChangedEventArgs());
         return result;
     }
 
     @Override
     public boolean addAll(int location, Collection<? extends T> collection) {
-        boolean result = this._wrappedList.addAll(location, collection);
+        boolean result = this.getWrappedList().addAll(location, collection);
         this._collectionChangedEvent.fire(new CollectionChangedEventArgs());
         return result;
     }
 
     @Override
     public boolean addAll(Collection<? extends T> collection) {
-        boolean result = this._wrappedList.addAll(collection);
+        boolean result = this.getWrappedList().addAll(collection);
         this._collectionChangedEvent.fire(new CollectionChangedEventArgs());
         return result;
     }
 
     @Override
     public void clear() {
-        this._wrappedList.clear();
+        this.getWrappedList().clear();
         this._collectionChangedEvent.fire(new CollectionChangedEventArgs());
     }
 
     @Override
     public boolean contains(Object object) {
-        return this._wrappedList.contains(object);
+        return this.getWrappedList().contains(object);
     }
 
     @Override
     public boolean containsAll(Collection<?> collection) {
-        return this._wrappedList.containsAll(collection);
+        return this.getWrappedList().containsAll(collection);
     }
 
     @Override
     public boolean equals(Object object) {
-        return this._wrappedList.equals(object);
+        return this.getWrappedList().equals(object);
     }
 
     @Override
     public T get(int location) {
-        return this._wrappedList.get(location);
+        return this.getWrappedList().get(location);
     }
 
     @Override
     public int hashCode() {
-        return this._wrappedList.hashCode();
+        return this.getWrappedList().hashCode();
     }
 
     @Override
     public int indexOf(Object object) {
-        return this._wrappedList.indexOf(object);
+        return this.getWrappedList().indexOf(object);
     }
 
     @Override
     public boolean isEmpty() {
-        return this._wrappedList.isEmpty();
+        return this.getWrappedList().isEmpty();
     }
 
     @Override
     public Iterator<T> iterator() {
-        return this._wrappedList.iterator();
+        return this.getWrappedList().iterator();
     }
 
     @Override
     public int lastIndexOf(Object object) {
-        return this._wrappedList.lastIndexOf(object);
+        return this.getWrappedList().lastIndexOf(object);
     }
 
     @Override
     public ListIterator<T> listIterator() {
-        return this._wrappedList.listIterator();
+        return this.getWrappedList().listIterator();
     }
 
     @Override
     public ListIterator<T> listIterator(int location) {
-        return this._wrappedList.listIterator(location);
+        return this.getWrappedList().listIterator(location);
     }
 
     @Override
     public T remove(int location) {
-        T result = this._wrappedList.remove(location);
+        T result = this.getWrappedList().remove(location);
         this._collectionChangedEvent.fire(new CollectionChangedEventArgs());
         return result;
     }
 
     @Override
     public boolean remove(Object object) {
-        boolean result = this._wrappedList.remove(object);
+        boolean result = this.getWrappedList().remove(object);
         this._collectionChangedEvent.fire(new CollectionChangedEventArgs());
         return result;
     }
 
     @Override
     public boolean removeAll(Collection<?> collection) {
-        boolean result = this._wrappedList.removeAll(collection);
+        boolean result = this.getWrappedList().removeAll(collection);
         this._collectionChangedEvent.fire(new CollectionChangedEventArgs());
         return result;
     }
 
     @Override
     public boolean retainAll(Collection<?> collection) {
-        boolean result = this._wrappedList.retainAll(collection);
+        boolean result = this.getWrappedList().retainAll(collection);
         this._collectionChangedEvent.fire(new CollectionChangedEventArgs());
         return result;
     }
 
     @Override
     public T set(int location, T object) {
-        T result = this._wrappedList.set(location, object);
+        T result = this.getWrappedList().set(location, object);
         this._collectionChangedEvent.fire(new CollectionChangedEventArgs());
         return result;
     }
 
     @Override
     public int size() {
-        return this._wrappedList.size();
+        return this.getWrappedList().size();
     }
 
     @Override
     public List<T> subList(int start, int end) {
-        return this._wrappedList.subList(start, end);
+        return this.getWrappedList().subList(start, end);
     }
 
     @Override
     public Object[] toArray() {
-        return this._wrappedList.toArray();
+        return this.getWrappedList().toArray();
     }
 
     @Override
     public <T1> T1[] toArray(T1[] array) {
-        return this._wrappedList.toArray(array);
+        return this.getWrappedList().toArray(array);
     }
 
     // </editor-fold>
+
+    protected List<T> getWrappedList()
+    {
+        return this._wrappedGetter.getObject();
+    }
 }
